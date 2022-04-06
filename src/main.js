df02c518f559355d13c6af76c7eb86e053b1becd
EmanuelaMocci <emanuelamocci2@gmail.com>
2022-04-06 12:55:03 +0200

    Add: font awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// font awesome
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import specific icons */
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* add icons to the library */
+library.add(faSearch);
+
+/* add font awesome icon component */
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
 import "./style/app.scss";
 
 Vue.config.productionTip = false;
